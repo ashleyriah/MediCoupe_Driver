274079d82b6e36951868c57630a8ba895902b539
Ashley Riah <ashley@ninthcoast.com>
2015-03-16 11:19:42 -0700

    Initial Commit

---
+++ MediCoupe_Driver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MediCoupe_Driver
+//
+//  Created by Ashley McMillion on 3/16/15.
+//  Copyright (c) 2015 Ninth Coast Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MediCoupe_DriverTests/MediCoupe_DriverTests.m
@@ -0,0 +1,40 @@
+//
+//  MediCoupe_DriverTests.m
+//  MediCoupe_DriverTests
+//
+//  Created by Ashley McMillion on 3/16/15.
+//  Copyright (c) 2015 Ninth Coast Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MediCoupe_DriverTests : XCTestCase
+
+@end
+
+@implementation MediCoupe_DriverTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
